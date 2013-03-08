5fb0c7cdd95f680fb9eb24d56eb8d192550f4630
Ryan Asher <rasher@tribune.com>
2013-03-07 20:32:52 -0600

    Complete level 11 exercises and update subsequent files.

---
+++ ex11-extra.py
@@ -0,0 +1,9 @@
+print "What ... is your name?",
+name = raw_input()
+print "What ... is your quest?",
+quest = raw_input()
+print "What ... is your favorite color?",
+color = raw_input()
+
+print "Your name is %r, your quest is %r, and your favorite color is %r." % (
+    name, quest, color)
