cdb7c5e328172b84d79bc6914f602a0374ce6980
Ryan Asher <rasher@tribune.com>
2013-04-13 15:12:11 -0500

    Finish exercise 19.

---
+++ ex19-extra1.py
@@ -0,0 +1,9 @@
+def star_wars(character1, character2):
+    print "Who is better %s, or %s?" % (character1, character2)
+    print "Clearly, %s is better." % character1
+    
+print "Preaching the truth, so that everyone knows:"
+character1 = "Boba Fett"
+character2 = "Han Solo"
+
+star_wars(character1, character2)
