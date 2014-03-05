8758202fc1929520d73c9b48bf0afc97c282e9f5
Ryan Asher <rasher@tribune.com>
2014-03-05 13:03:08 -0600

    Finish example 3 study drills, and example 4.

--- ex3.py
+++ ex3.py
@@ -1,28 +1,28 @@
 print "I will now count my chickens:"
 
 # Divide 30 by 6 and add 25.
-print "Hens", 25 + 30 / 6
+print "Hens", 25.0 + 30.0 / 6.0
 # Multiply 25 by 3, modulo by 4 to find the remainder and subtract against 100.
-print "Roosters", 100 - 25 * 3 % 4
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
 print "Now I will count the eggs:"
 
 # Modulo 4 and 2, divide 1 by 4, then do remaining addition and subtraction.
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 print "Is it true that 3 + 2 < 5 - 7?"
 
 # Compare if 5 is less than -2, which evaluates to boolean False.
-print 3 + 2 < 5 - 7
+print 3.0 + 2.0 < 5.0 - 7.0
 
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7?", 5 - 7
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7?", 5.0 - 7.0
 
 print "Oh, that's why it's False."
 
 print "How about some more."
 
 # Print boolean result for various evaluations below.
-print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5 >= -2
-print "Is it less or equal?", 5 <= -2
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
