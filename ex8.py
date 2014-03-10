ffb9c359ca150c3107e5081ee2cff670ca9cd413
Ryan Asher <rasher@tribune.com>
2014-03-10 12:52:37 -0500

    Finish exercises 7, 8, and 9.

---
+++ ex8.py
@@ -0,0 +1,14 @@
+formatter = "%r %r %r %r"
+
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+    "I had this thing.",
+    "That you could type up right.",
+    "But it didn't sing.",
+    "So I said goodnight."
+)
+
+# Study drill 2: This series of strings alternates between double and single quotes in order to escape the single quote being set in the third string.
