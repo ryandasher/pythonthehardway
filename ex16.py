9ef7f643dbb5effb460518d415d16215f03edc19
Ryan Asher <rasher@tribune.com>
2014-03-21 12:48:09 -0500

    Finish exercise 16 study drills.

--- ex16.py
+++ ex16.py
@@ -22,12 +22,16 @@ line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(line1 + '\n' + line2 + '\n' + line3 + '\n')
+
+print "Now we're going to show you what you wrote:"
+
+target = open(filename)
+print target.read()
 
 print "And finally, we close it."
 target.close()
+
+# Study drill 4: You presumably have to pass the 'w' manually because opening the file in write mode will truncate it.
+
+# Study drill 5: Yes, opening the file in write mode will truncate it.
